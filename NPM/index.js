02777c4083921840089bcf679f9d4105b60cc2c7
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2024-10-17 23:24:11 +0700

    day7

---
+++ NPM/index.js
@@ -0,0 +1,3 @@
+import { writeToFile } from "./write.js";
+
+writeToFile("./Hello.log", "Lutfiya Ainurrahman Prasetyp");
---
+++ NPM/lodash.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const name = "lutfiya ainurrahman prasetyo";
+const read = _.upperCase(name);
+
+console.info(read);
