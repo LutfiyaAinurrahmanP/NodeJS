2c5dfdd6b8e418a1231b5fd23fe42d6cf9085b78
Lutfiya Ainurrahman Prasetyo <lutfiyapr.stu@pnc.ac.id>
2024-10-18 20:44:34 +0700

    day8

---
+++ UnitTest/test/sum.test.js
@@ -0,0 +1,19 @@
+import { sum } from "../src/sum.js";
+
+test('function1', () => {
+    const result = sum(1, 2);
+
+    expect(result).toBe(3);
+});
+
+test('function 2', () => {
+    const result = sum(1, 2);
+
+    expect(result).toBe(3);
+});
+
+test('function 3', () => {
+    const result = sum(1, 2);
+
+    expect(result).toBe(3);
+});
